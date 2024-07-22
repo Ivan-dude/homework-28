bd603d41ef44389cfac180c34fef3244bf81be32
Ivan-dude <ivandude87@mail.ru>
2024-07-22 18:53:24 +1000

    Add files via upload

--- module_7_1.py
+++ module_7_1.py
@@ -8,19 +8,20 @@ class Product:
         return f'{self.name}, {self.weight}, {self.category}'
 class Shop():
     __file_name = 'products.txt'
+
     def get_product(self):
-        self.file = open(self.__file_name, 'r')
-        list_of_products = self.file.read()
-        self.file.close()
+        file = open(self.__file_name, 'r')
+        list_of_products = file.read()
+        file.close()
         return list_of_products
 
     def add(self, *products):
         for i in products:
+            file = open(self.__file_name, 'a')
             list_of_products = self.get_product()
             if i.name not in list_of_products:
-                self.file = open(self.__file_name, 'a')
-                self.file.write(f'{i}\n')
-                self.file.close()
+                file.write(f'{i}\n')
+                file.close()
             else:
                 print(f'Продукт {i.name} уже есть в магазине')
 
@@ -31,5 +32,7 @@ p3 = Product('Potato', 5.5, 'Vegetables')
 print(p2) # __str__
 s1.add(p1, p2, p3)
 print(s1.get_product())
+
+
 
 
